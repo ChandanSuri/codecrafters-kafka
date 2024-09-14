462437dc739aab6fa68234e91d82f57dbac65a66
Chandan Suri <csuri21296@gmail.com>
2024-09-14 16:53:43 -0400

    send correlation ID

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
 import socket  # noqa: F401
 
-port_number = 9092
+class Broker:
+    def __init__(self, port_number, reuse_port) -> None:
+        self.server = socket.create_server(("localhost", port_number), reuse_port=reuse_port)
+        self.client = None
+        self.address = None
+
+    def stream(self):
+        while True:
+            self.client, self.address = self.server.accept()
+            self.handle_request()
+
+    def handle_request(self):
+        self.client.recv(1024)
+        self.client.sendall(self.create_response(7))
+        self.client.close()
+
+    def create_response(self, message: int):
+        msg_bytes = message.to_bytes(4, byteorder="big", signed=True)
+        len_bytes = len(msg_bytes).to_bytes(4, byteorder="big", signed=True)
+        return len_bytes + msg_bytes
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server = socket.create_server(("localhost", port_number), reuse_port=True)
-    server.accept() # wait for client
-
+    broker = Broker(9092, True)
+    broker.stream()
 
 if __name__ == "__main__":
     main()
