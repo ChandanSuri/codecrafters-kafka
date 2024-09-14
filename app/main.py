f58ad8cf14627cda5bc30d28bba8ba32ebf4a14b
Chandan Suri <csuri21296@gmail.com>
2024-09-14 18:43:55 -0400

    added changes for parsing correlation id

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 import socket  # noqa: F401
 
+class Parser:
+    def __init__(self, ) -> None:
+        self.correlationBytesIndices = [8, 12]
+
+    def get_correlation_id(self, client_response):
+        return int.from_bytes(
+            client_response[
+                self.correlationBytesIndices[0]:
+                self.correlationBytesIndices[1]
+            ], 
+            byteorder="big"
+        )
+
 class Broker:
     def __init__(self, port_number, reuse_port) -> None:
         self.server = socket.create_server(("localhost", port_number), reuse_port=reuse_port)
+        self.parser = Parser()
         self.client = None
         self.address = None
 
@@ -12,8 +26,9 @@ class Broker:
             self.handle_request()
 
     def handle_request(self):
-        self.client.recv(1024)
-        self.client.sendall(self.create_response(7))
+        client_response = self.client.recv(1024)
+        correlation_id = self.parser.get_correlation_id(client_response)
+        self.client.sendall(self.create_response(correlation_id))
         self.client.close()
 
     def create_response(self, message: int):
