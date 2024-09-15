e82ea948d73acce18a02691ebf89ca608590aa2b
Chandan Suri <csuri21296@gmail.com>
2024-09-14 20:43:35 -0400

    fixed a bug

--- app/main.py
+++ app/main.py
@@ -66,7 +66,7 @@ class Broker:
 
     def create_response(self, header, body):
         msg_len = len(header) + len(body)
-        len_bytes = len(msg_len).to_bytes(4, byteorder="big", signed=True)
+        len_bytes = msg_len.to_bytes(4, byteorder="big", signed=True)
         response = len_bytes + header + body
         return response
 
